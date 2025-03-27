31a1408aa7ff2ab5ef6f14dca9fe18ea8db03891
Malkiworks <your-email@example.com>
2025-03-27 18:08:35 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export",
   images: {
     domains: ['images.unsplash.com'], // Add any external image domains here
     unoptimized: true, // Required for static export
